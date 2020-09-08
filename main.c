c9efa7ba9a28e18573e5a2c088295a0f0f1034c9
Sree Revoori <srrevoori@gmail.com>
2020-09-08 08:53:17 -0400

    Update main.c

--- main.c
+++ main.c
@@ -17,6 +17,12 @@ void enableRawMode() {
 int main() {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
