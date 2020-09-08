febeeec54e97a424cd76e0fa9ff8ad425e8f0f3a
Sree Revoori <srrevoori@gmail.com>
2020-09-08 08:59:44 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+/*** data ***/
+
 struct termios orig_termios;
 
+/*** terminal ***/
 
 void die(const char *s) {
   perror(s);
@@ -31,6 +37,7 @@ void enableRawMode() {
 
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+/*** init ***/
 
 int main() {
   enableRawMode();
