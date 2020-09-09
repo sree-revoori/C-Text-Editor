b5e0eaaf2dfad540a4b65a17112f80f03e9b7506
Sree Revoori <srrevoori@gmail.com>
2020-09-09 08:57:49 -0400

    Update main.c

--- main.c
+++ main.c
@@ -51,6 +51,12 @@ char editorReadKey() {
   return c;
 }
 
+
+/*** output ***/
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -67,6 +73,7 @@ void editorProcessKeypress() {
 int main() {
   enableRawMode();
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
   return 0;
