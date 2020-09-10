ed6c9acfb7546c2fbd3a6f4fe7e40a2efc5363c6
Sree Revoori <srrevoori@gmail.com>
2020-09-10 08:51:00 -0400

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,10 @@
 
 
 /*** defines ***/
+#define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
+
 
 /*** data ***/
 
@@ -117,7 +120,15 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
     
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
@@ -125,6 +136,7 @@ void editorDrawRows(struct abuf *ab) {
     }
   }
 }
+
 
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
