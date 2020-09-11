869d923bf1eeb1b665e82ae7ac0d287141e6ddd4
Sree Revoori <srrevoori@gmail.com>
2020-09-11 10:01:38 -0400

    Update main.c

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
@@ -87,6 +88,7 @@ int editorReadKey() {
         if (seq[2] == '~') {
           switch (seq[1]) {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
@@ -249,7 +251,15 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
+      break;  
+    
+    case HOME_KEY:
+      E.cx = 0;
       break;
+      
+    case END_KEY:
+      E.cx = E.screencols - 1;
+      break;  
       
     case PAGE_UP:
     case PAGE_DOWN:
