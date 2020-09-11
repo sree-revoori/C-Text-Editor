144b9bb73fb03041f14f13f810f5ba66902e4502
Sree Revoori <srrevoori@gmail.com>
2020-09-11 09:57:32 -0400

    Update main.c

--- main.c
+++ main.c
@@ -19,7 +19,10 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
+    
 };
 
 
@@ -72,16 +75,27 @@ int editorReadKey() {
   
     if (c == '\x1b') {
     char seq[3];
+      
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
       
     if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+          }
+        }
+      } else {
       switch (seq[1]) {
         case 'A': return ARROW_UP;
         case 'B': return ARROW_DOWN;
         case 'C': return ARROW_RIGHT;
         case 'D': return ARROW_LEFT;
       }
+    }
     }
     return '\x1b';
   } else {
