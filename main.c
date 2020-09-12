83cb59f0edd88747f6f9a9de8115b118d5f91c7d
Sree Revoori <srrevoori@gmail.com>
2020-09-12 11:32:45 -0400

    Update main.c

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ struct editorConfig {
   int screenrows;
   int screencols;
   int numrows;
-  erow row;
+  erow *row;
   struct termios orig_termios;
 };
 struct editorConfig E;
@@ -327,6 +327,7 @@ void initEditor() {
   E.cx = 0;
   E.cy = 0;
   E.numrows = 0;
+  E.row = NULL;
   
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
