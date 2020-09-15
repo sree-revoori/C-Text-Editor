0a3e21a09bbf049fe728eb7cdc86db873215f7a9
Sree Revoori <srrevoori@gmail.com>
2020-09-15 08:27:03 -0400

    Update main.c

--- main.c
+++ main.c
@@ -21,6 +21,7 @@
 /*** defines ***/
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -494,6 +495,8 @@ switch (key) {
 }
 
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
+  
   int c = editorReadKey();
   
   switch (c) {
@@ -502,6 +505,12 @@ void editorProcessKeypress() {
       break;
       
     case CTRL_KEY('q'):
+      if (E.dirty && quit_times > 0) {
+        editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+          "Press Ctrl-Q %d more times to quit.", quit_times);
+        quit_times--;
+        return;
+      }
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
@@ -557,6 +566,8 @@ void editorProcessKeypress() {
       editorInsertChar(c);
       break;
   }
+ 
+  quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
