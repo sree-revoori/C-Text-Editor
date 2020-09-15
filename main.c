1aa7a19f93cb63c757efd7b1c28cefac7a344368
Sree Revoori <srrevoori@gmail.com>
2020-09-15 08:18:59 -0400

    Update main.c

--- main.c
+++ main.c
@@ -23,6 +23,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+  BACKSPACE = 127,
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
@@ -443,6 +444,10 @@ void editorProcessKeypress() {
   int c = editorReadKey();
   
   switch (c) {
+    case '\r':
+      /* TODO */
+      break;
+      
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
@@ -457,6 +462,12 @@ void editorProcessKeypress() {
       if (E.cy < E.numrows)
         E.cx = E.row[E.cy].size;
       break;  
+      
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
+      /* TODO */
+      break;
       
     case PAGE_UP:
     case PAGE_DOWN:
@@ -481,6 +492,9 @@ void editorProcessKeypress() {
       editorMoveCursor(c);
       break;
       
+    case CTRL_KEY('l'):
+    case '\x1b':
+      break;
       
     default:
       editorInsertChar(c);
