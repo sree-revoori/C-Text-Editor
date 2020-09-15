98dbbe07be5dcf6ba002631e40ae21b91944e4a5
Sree Revoori <srrevoori@gmail.com>
2020-09-15 08:23:41 -0400

    Update main.c

--- main.c
+++ main.c
@@ -57,12 +57,18 @@ struct editorConfig {
   int screencols;
   int numrows;
   erow *row;
+  int dirty;
   char *filename;
   char statusmsg[80];
   time_t statusmsg_time;
   struct termios orig_termios;
 };
+
 struct editorConfig E;
+
+/*** prototypes ***/
+
+void editorSetStatusMessage(const char *fmt, ...);
 
 /*** terminal ***/
 
@@ -558,6 +564,7 @@ void initEditor() {
   E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.dirty = 0;
   E.filename = NULL;
   E.statusmsg[0] = '\0';
   E.statusmsg_time = 0;
