17df82283d25a7978a8ef453eea0095e63f420cc
Sree Revoori <srrevoori@gmail.com>
2020-09-16 10:05:41 -0400

    Update main.c

--- main.c
+++ main.c
@@ -71,6 +71,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 /*** terminal ***/
 
@@ -361,7 +362,9 @@ void editorOpen(char *filename) {
 }
 
 void editorSave() {
-  if (E.filename == NULL) return;
+  if (E.filename == NULL) {
+    E.filename = editorPrompt("Save as: %s");
+  }
   
   int len;
   char *buf = editorRowsToString(&len);
