6027cbfbc2d989d9dcef244a92831d6719cd40c0
Sree Revoori <srrevoori@gmail.com>
2020-09-17 08:37:55 -0400

    Update main.c

--- main.c
+++ main.c
@@ -377,7 +377,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
   if (E.filename == NULL) {
-    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
     if (E.filename == NULL) {
       editorSetStatusMessage("Save aborted");
       return;
@@ -387,7 +387,7 @@ void editorSave() {
   /*** find ***/
   
 void editorFind() {
-  char *query = editorPrompt("Search: %s (ESC to cancel)");
+  char *query = editorPrompt("Search: %s (ESC to cancel)", NULL);
   if (query == NULL) return;
   int i;
   
