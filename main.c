21d4e48afd22c7feccccf02a08d468310a711d72
Sree Revoori <srrevoori@gmail.com>
2020-09-19 14:15:29 -0400

    Update main.c

--- main.c
+++ main.c
@@ -225,6 +225,8 @@ void editorUpdateSyntax(erow *row) {
   row->hl = realloc(row->hl, row->rsize);
   memset(row->hl, HL_NORMAL, row->rsize);
   
+  if (E.syntax == NULL) return;
+  
   int prev_sep = 1;
   
   int i = 0;
@@ -232,13 +234,14 @@ void editorUpdateSyntax(erow *row) {
     while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
-      
+    if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {  
     if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
         (c == '.' && prev_hl == HL_NUMBER)) {
       row->hl[i] = HL_NUMBER;
       i++;
       prev_sep = 0;
       continue;
+    }
     }
     
     prev_sep = is_separator(c);
