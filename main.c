a15d43b5f1bede4cd0e29f696ab59920da7cdbd6
Sree Revoori <srrevoori@gmail.com>
2020-09-20 10:29:07 -0400

    Update main.c

--- main.c
+++ main.c
@@ -57,6 +57,7 @@ enum editorHighlight {
 struct editorSyntax {
   char *filetype;
   char **filematch;
+  char *singleline_comment_start;
   int flags;
 };
 
@@ -95,6 +96,7 @@ struct editorSyntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
+    "//",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
